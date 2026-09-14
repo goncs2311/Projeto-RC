c60a8f63bcf57d73797f429ad92beb23548c19e7
Gonçalo Carrasquinho <asus@LAPTOP-Q8EM2C80>
2026-09-14 16:42:01 +0100

    VEM COMEEEEER

--- user.c
+++ user.c
@@ -8,11 +8,69 @@
 #include <string.h>
 #include <stdio.h>
 
-#define PORT "58001"    // the port (from the server) that users will be connecting to
+#define DSIP "193.136.138.142"
+#define DSPORT 59000
 
-void readCommand(char* input, char* command, int *uid, int *tcpport, char* password) {
-    sscanf(input, "%s %d %d %s", command, uid, tcpport, password);
+int main(int argc, char *argv[]) {
+    char dsip[16];
+    int tcpport, dsport = DSPORT;
+    
+    char input[128];
+    char command[3];
+    int uid;
+    char password[9];
+
+    tcpport = atoi(argv[2]);
+    if (argc > 3) {
+        strcpy(dsip, argv[4]); // verify if valid
+    }
+    if (argc > 5) {
+        dsport = atoi(argv[6]); // verify if valid
+    }
+
+
+
+    int fd, errcode;
+    ssize_t n;
+    socklen_t addrlen;
+    struct addrinfo hints, *res;    // hints are the address info from the user, res is the address info from the server
+    struct sockaddr_in addr;    // address from whoever sent the message (server or client)
+    char buffer[128];
+
+    //starting UDP socket
+    fd= socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd==-1) /*error*/exit(1);
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family=AF_INET;
+    hints.ai_socktype=SOCK_DGRAM;
+
+    //getting the server address
+    errcode=getaddrinfo("tejo.tecnico.ulisboa.pt", DSPORT, &hints, &res);
+
+
+    //printf("tcpport: %d, dsip: %s, dsport: %d\n", tcpport, dsip, dsport);
+
+    fgets(input, 128, stdin);
+    // ler só as primeiras 3 letras (comando)
+    strncpy(command, input, 3);
+    command[3] = '\0';
+
+    if (strcmp(command, "LIN") == 0) {
+        // login(uid, password, tcpport);
+        sscanf(input, "LIN %d %s %d", &uid, password, &tcpport);
+        
+    } else if (strcmp(command, "RLI") == 0) {
+        // register(uid, tcpport, password);
+    } else {
+        printf("Invalid command\n");
+    }
+
+    printf("command: %s", command);
+
+    return 0;
 }
+
 
 int udp() {
     int fd, errcode;
@@ -43,44 +101,6 @@ int udp() {
 
     freeaddrinfo(res);
     close(fd);
-
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
-    char input[128];
-    char command[3];
-    char dsip[16];
-    int uid, tcpport, dsport;
-    char password[9];
-
-    tcpport = atoi(argv[2]);
-    if (argc > 3) {
-        strcpy(dsip, argv[4]);
-    }
-    if (argc > 5) {
-        dsport = atoi(argv[6]);
-    }
-
-    printf("tcpport: %d, dsip: %s, dsport: %d\n", tcpport, dsip, dsport);
-
-    fgets(input, 128, stdin);
-    // ler só as primeiras 3 letras (comando)
-    strncpy(command, input, 3);
-    command[3] = '\0';
-    // depois guardar o resto em variaveis
-    readCommand(input, command, &uid, &tcpport, password);
-
-    
-    if (strcmp(command, "LIN", 0) == 0) {
-        // login(uid, tcpport, password);
-    } else if (strcmp(command, "RLI", 0) == 0) {
-        // register(uid, tcpport, password);
-    } else {
-        printf("Invalid command\n");
-    }
-
-    printf("command: %s", input);
 
     return 0;
 }
